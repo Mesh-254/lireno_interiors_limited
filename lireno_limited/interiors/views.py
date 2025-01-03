7754da6ffcf9d5f46d7b19f878fe8083933f63f8
Mesh-254 <meshack1995@gmail.com>
2025-01-03 16:51:09 +0300

    Add views for Category List and Detail functionalities.

--- lireno_limited/interiors/views.py
+++ lireno_limited/interiors/views.py
@@ -1,3 +1,48 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Category
+from .serializers import CategorySerializer
 
-# Create your views here.
+
+@api_view(['GET', 'POST'])
+def category_list(request):
+    if request.method == 'GET':
+        try:
+            categories = Category.objects.all()
+            serializer = CategorySerializer(categories, many=True)
+        except Category.DoesNotExist:
+            return Response(status=404)
+        
+        return Response(serializer.data)
+    
+
+    elif request.method == 'POST':
+        serializer = CategorySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def category_detail(request, pk):
+    try:
+        category = Category.objects.get(pk=pk)
+    except Category.DoesNotExist:
+        return Response(status=404)
+
+    if request.method == 'GET':
+        serializer = CategorySerializer(category)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = CategorySerializer(category, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        category.delete()
+        return Response(status=204)
