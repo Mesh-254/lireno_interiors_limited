672ee2b8990835936cbf3a6c7983450fc18f3403
Mesh-254 <meshack1995@gmail.com>
2025-01-05 16:22:07 +0300

    Update URL paths for  and related views

--- lireno_limited/interiors/urls.py
+++ lireno_limited/interiors/urls.py
@@ -3,6 +3,24 @@ from interiors import views
 
 
 urlpatterns = [
-    path('categories/', views.category_list),
-    path('categories/<str:pk>/', views.category_detail),
+    path('categories/', views.CategoryList.as_view()),
+    path('categories/<str:pk>/', views.CategoryDetail.as_view()),
+
+
+    path('products/list/', views.ProductList.as_view()),
+    path('products/create/', views.ProductCreate.as_view()),
+    path('products/<str:pk>/', views.ProductDetail.as_view()),
+
+    path('suppliers/list/', views.SupplierList.as_view()),
+    path('suppliers/<str:pk>/', views.SupplierDetail.as_view()),
+
+    path('stocks/list/', views.StockList.as_view()),
+    path('stocks/<str:pk>/', views.StockDetail.as_view()),
+
+    path('purchases/list/', views.PurchaseList.as_view()),
+    path('purchases/<str:pk>/', views.PurchaseDetail.as_view()),
+
+    # path('sales/list/', views.SaleList.as_view()),
+    # path('sales/<str:pk>/', views.SaleDetail.as_view()),
+
 ]
