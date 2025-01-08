6aea574d0d3c4d21ca980aca2922a13c504c56a4
Mesh-254 <meshack1995@gmail.com>
2025-01-08 16:19:33 +0300

    created url patterns names

--- lireno_limited/interiors/urls.py
+++ lireno_limited/interiors/urls.py
@@ -4,30 +4,26 @@ from interiors import views
 
 
 urlpatterns = [
-    path('categories/list/', views.CategoryList.as_view()),
-    path('categories/<str:pk>/', views.CategoryDetail.as_view()),
+    path('categories/list/', views.CategoryList.as_view(), name='category-list'),
+    path('categories/<str:pk>/', views.CategoryDetail.as_view(), name='category-detail'),
 
+    path('products/list/', views.ProductList.as_view(), name='product-list'),
+    path('products/<str:pk>/', views.ProductDetail.as_view(), name='product-detail'),
 
-    path('products/list/', views.ProductList.as_view()),
-    path('products/create/', views.ProductCreate.as_view()),
-    path('products/<str:pk>/', views.ProductDetail.as_view()),
+    path('suppliers/list/', views.SupplierList.as_view(), name='supplier-list'),
+    path('suppliers/<str:pk>/', views.SupplierDetail.as_view(), name='supplier-detail'),
 
-    path('suppliers/list/', views.SupplierList.as_view()),
-    path('suppliers/<str:pk>/', views.SupplierDetail.as_view()),
+    path('stocks/list/', views.StockList.as_view(), name='stock-list'),
+    path('stocks/<str:pk>/', views.StockDetail.as_view(), name='stock-detail'),
 
-    path('stocks/list/', views.StockList.as_view()),
-    path('stocks/<str:pk>/', views.StockDetail.as_view()),
+    path('purchases/list/', views.PurchaseList.as_view(), name='purchase-list'),
+    path('purchases/<str:pk>/', views.PurchaseDetail.as_view(), name='purchase-detail'),
 
-    path('purchases/list/', views.PurchaseList.as_view()),
-    path('purchases/<str:pk>/', views.PurchaseDetail.as_view()),
+    path('sales/list/', views.SaleList.as_view(), name='sale-list'),
+    path('sales/<str:pk>/', views.SaleDetail.as_view(), name='sale-detail'),
 
-    path('sales/list/', views.SaleList.as_view()),
-    path('sales/<str:pk>/', views.SaleDetail.as_view()),
-
-
-    path('users/list/', views.UserList.as_view()),
-    path('users/<str:pk>/', views.UserDetail.as_view()),
+    path('users/list/', views.UserList.as_view(), name='user-list'),
+    path('users/<str:pk>/', views.UserDetail.as_view(), name='user-detail'),
 
     path('auth/', include('rest_framework.urls')),
-
 ]
